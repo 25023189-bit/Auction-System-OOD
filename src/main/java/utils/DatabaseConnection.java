321725f263397e8a8e888a252fb8fa59cef33885
DUNGBUI <25023189@vnu.edu.vn>
2026-03-26 10:33:00 +0700

    feat: Hoan thanh bam mat khau va ket noi MySQL

---
+++ src/main/java/utils/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // Tên database của bạn là auction_db (dựa theo file SQL V5)
+    private static final String URL = "jdbc:mysql://localhost:3306/auction_system";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
