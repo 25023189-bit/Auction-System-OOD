ca7b507c11503b486e4ee90d69dcc278de689ea8
NguyenHuuHung <25023262@vnu.edu.vn>
2026-04-07 22:29:06 +0700

    Fix pom.xml and module, admin

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,13 +4,13 @@ module com.example.auctionprototype {
     requires java.sql;
     requires jbcrypt;
 
-    // Cấp quyền cho JavaFX đọc các file Controller
+    opens com.auction.common.model to javafx.base;
+
     opens com.auction.client.controllers to javafx.fxml;
 
-    // QUAN TRỌNG: Cấp quyền cho JavaFX khởi tạo class Main
     opens com.auction.server.main to javafx.graphics, javafx.fxml;
     opens com.example.auctionprototype to javafx.fxml;
 
     exports com.auction.client;
-    exports com.auction.server.main; // Export nếu cần thiết
+    exports com.auction.server.main;
 }
